66819e30a273e09e0cb73616df22df91b9545a19
ppzarebski@gmail.com <Kolbuszowa12151>
2021-05-31 22:43:02 +0200

    web module introduced

---
+++ tests-demo-web/src/main/java/com/github/ppzarebski/qa/web/driver/BrowserName.java
@@ -0,0 +1,11 @@
+package com.github.ppzarebski.qa.web.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public enum BrowserName {
+  CHROME, FIREFOX, OPERA;
+}
