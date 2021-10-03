7db58dc4ed986af22714fc71ff8e1ee1dea936b0
ppzarebski@gmail.com <Kolbuszowa12151>
2021-10-03 20:14:03 +0200

    websocket connection fixed; message sending to be fixed

--- tests-demo-performance/src/test/scala/com/github/ppzarebski/qa/performance/tests/Runner.scala
+++ tests-demo-performance/src/test/scala/com/github/ppzarebski/qa/performance/tests/Runner.scala
@@ -1,12 +1,12 @@
 package com.github.ppzarebski.qa.performance.tests
 
-import com.github.ppzarebski.qa.performance.tests.petstore.FindPetByStatusSimulation
+import com.github.ppzarebski.qa.performance.tests.echowebsocket.SendMessageSimulation
 
 object Runner extends BaseRunner {
 
   /**
-    * / paste simulation class name in method below to run the simulation with your IDE
-    */
-  runSimulation(classOf[FindPetByStatusSimulation].getName)
+   * / paste simulation class name in method below to run the simulation with your IDE
+   */
+  runSimulation(classOf[SendMessageSimulation].getName)
 
 }
--- tests-demo-performance/src/test/scala/com/github/ppzarebski/qa/performance/tests/echowebsocket/SendMessageSimulation.scala
+++ tests-demo-performance/src/test/scala/com/github/ppzarebski/qa/performance/tests/echowebsocket/SendMessageSimulation.scala
@@ -4,24 +4,27 @@ import com.github.ppzarebski.qa.commons.helper.RandomGenerator
 import com.github.ppzarebski.qa.performance.tests.BaseHttpProtocol
 import com.github.ppzarebski.qa.websocket.config.ApiConfig.WS_HOST
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.check.ws.WsTextFrameCheck
+import io.gatling.http.protocol.HttpProtocolBuilder
 
-//TODO: websocket connection to be fixed
+//TODO: websocket message sending to be fixed
 class SendMessageSimulation extends Simulation {
 
-  val wsProtocol = BaseHttpProtocol.wsProtocol(WS_HOST)
-  var wsName: String = null
+  val wsProtocol: HttpProtocolBuilder = BaseHttpProtocol.wsProtocol(WS_HOST)
+  var message: String = getMessage().toString
+  val conditionCheck: WsTextFrameCheck = ws.checkTextMessage("message check").check(regex(message))
 
-  val sendMessageSimulation = scenario("Connect to websocket and send message")
-    .exec(
-      ws("connect ws and send message").wsName(getWsName().toString())
-        .connect("/")
-        .onConnected(
-          exec(ws("send message").wsName(wsName).sendText(RandomGenerator.faker.chuckNorris().fact()))
-        ))
+  val sendMessageSimulation: ScenarioBuilder = scenario("Connect to websocket and send message")
+    .exec(ws("ws")
+      .connect("/")
+      .onConnected(
+        exec(ws("ws").sendText(message).await(5)(conditionCheck))
+      ))
   setUp(sendMessageSimulation.inject(atOnceUsers(1))).protocols(wsProtocol)
 
-  def getWsName(): Unit = {
-    wsName = RandomGenerator.faker.pokemon().name() + RandomGenerator.getRandomInt(10, 999).toString
+  def getMessage(): Unit = {
+    message = RandomGenerator.faker.chuckNorris().fact()
   }
 }
